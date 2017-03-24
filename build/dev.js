3faa70d1523e74629e0914f75d30d031d3ab6fcb
junyiz <junyime@qq.com>
2017-03-24 15:51:35 +0800

    support the year of change

--- build/dev.js
+++ build/dev.js
@@ -17,7 +17,8 @@ module.exports = merge(baseConf, {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map',
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
   plugins: [
     new HtmlwebpackPlugin({
       template: './src/index.html',
